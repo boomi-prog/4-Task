573f33cd18a5e983e29a8beabb2b2234cd9370b6
boomi-prog <boomikabr07@gmail.com>
2025-05-22 16:35:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showCourse() {
+  document.getElementById("course-detail").classList.remove("hidden");
+}
+function showDataScienceCourse() {
+  document
+    .getElementById("datascience-course-detail")
+    .classList.remove("hidden");
+}
+function showAICourse() {
+  document.getElementById("ai-course-detail").classList.remove("hidden");
+}
+function showCyberCourse() {
+  document.getElementById("cyber-course-detail").classList.remove("hidden");
+}
